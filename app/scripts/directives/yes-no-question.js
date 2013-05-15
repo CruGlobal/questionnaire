86eb54348bf25f8fb8cc73ed0db26b00e1ab2c83
robbyronk <robby.ronk@gmail.com>
2013-05-15 10:37:30 +0000

    Initial dist commit

---
+++ app/scripts/directives/yes-no-question.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular.module('questionnaireApp')
+  .directive('yesNoQuestion', function () {
+    return {
+      template: '<input type="checkbox" data-ng-model="answer" data-ng-change="updateAnswer()">',
+      link: function postLink(scope, element, attrs) {
+        scope.updateAnswer = function() {
+          scope.answers[scope.block.name] = scope.answer;
+        }
+      }
+    };
+  });
