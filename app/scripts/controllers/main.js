bc866a6865d8fa532ad3252338f3c3bdc21b114a
robbyronk <robby.ronk@gmail.com>
2013-05-15 06:29:04 +0000

    `yo angular`

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('questionnaireApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
